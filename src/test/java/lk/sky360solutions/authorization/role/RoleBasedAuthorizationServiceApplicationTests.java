d7856a664a99fd18f29c49536c48b019846daa3e
skspyapa <supun@rumex.lk>
2021-05-17 20:47:52 +0530

    Add spring initializer project

---
+++ src/test/java/lk/sky360solutions/authorization/role/RoleBasedAuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sky360solutions.authorization.role;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedAuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
